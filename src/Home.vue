21eb498dd823f8c271fbf1c918bf15e3e1e08bf5
Piotr Pupczyk <p.pupczyk@samsung.com>
2019-01-02 18:00:42 +0100

    move exercises to file

--- src/Home.vue
+++ src/Home.vue
@@ -50,6 +50,7 @@
 
 <script>
 import Exercise from "./components/Exercise";
+import { exercises } from './exercises.js';
 
 export default {
   name: "Home",
@@ -57,93 +58,7 @@ export default {
   data: function() {
     return {
       accordionIsOpen: false,
-      exercises: [
-        {
-          type: "Push",
-          name: "Wyciskanie sztangi leżąc na ławce",
-          quantity: "3x10"
-        },
-        {
-          type: "Push",
-          name: "Wyciskanie hantli skos",
-          quantity: "3x10"
-        },
-        {
-          type: "Push",
-          name: "Rozpiętki skos",
-          quantity: "3x10"
-        },
-        {
-          type: "Push",
-          name: "Wyciskanie żołnierskie",
-          quantity: "3x8"
-        },
-        {
-          type: "Push",
-          name: "Wznosy bokiem hantli",
-          quantity: "3x12"
-        },
-        {
-          type: "Push",
-          name: "Francuz, hantle zza głowy",
-          quantity: "3x8"
-        },
-        {
-          type: "Pull",
-          name: "Wiosłowanie sztangą w opadzie",
-          quantity: "3x8"
-        },
-        {
-          type: "Pull",
-          name: "Wiosłowanie hantlem w podporze",
-          quantity: "3x10"
-        },
-        {
-          type: "Pull",
-          name: "Podciąganie na drążku",
-          quantity: "3x8"
-        },
-        {
-          type: "Pull",
-          name: "Wznosy w opadzie tułowia / facepull",
-          quantity: "3x12"
-        },
-        {
-          type: "Pull",
-          name: "Wznosy bokiem na bramie",
-          quantity: "3x12"
-        },
-        {
-          type: "Pull",
-          name: "Uginanie przedramion ze sztangą",
-          quantity: "3x8"
-        },
-        {
-          type: "Legs",
-          name: "Przysiad ze sztangą",
-          quantity: "3x10"
-        },
-        {
-          type: "Legs",
-          name: "Martwy ciąg",
-          quantity: "3x8"
-        },
-        {
-          type: "Legs",
-          name: "Przysiad bułgarski",
-          quantity: "3x10"
-        },
-        {
-          type: "Legs",
-          name: "Uginanie nóg leżąc",
-          quantity: "3x10"
-        },
-        {
-          type: "Legs",
-          name: "Hip Trust",
-          quantity: "3x8"
-        }
-      ]
+      exercises: exercises
     };
   },
   methods: {
